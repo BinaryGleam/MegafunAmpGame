6aa3fd6457a04fe897f6b1d80150489263b8efcd
Lukas TAMAYO <luknight@hotmail.fr>
2021-03-22 18:40:12 +0100

    [LukasPiggy]<Animation> Placeholder animations
    -Did some placeholder animation that just changes sprite color to indicate current animation state.
    -Did an animator
    -Added it to the new animator component of our Piggy <3
    -Created the scriptableitem scriptableobject for future puzzles
    -Added a bunch of variable and enums to the character
    
    Im so tired

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum PlayerState
+{
+    NORMAL = 0x0,
+    HIDDEN,
+    GAMEOVER
+}
+
 public class Character : MonoBehaviour
 {
     [HideInInspector]
@@ -11,9 +18,13 @@ public class Character : MonoBehaviour
     [HideInInspector]
     public float speedMultiplier = 1f;
 
+    [HideInInspector]
+    public ScriptableItem currentItem = null;
+
     [SerializeField]
     private float speed = 1f, jumpForce = 1f, runSpeedMultiplier = 1f;
 
+    private PlayerState currentState = PlayerState.NORMAL;
 
     public float Speed
 	{
@@ -28,5 +39,20 @@ public class Character : MonoBehaviour
     public float RunSpeedMultiplier
 	{
         get { return runSpeedMultiplier; }
+	}
+
+	private void Update()
+	{
+		switch (currentState)
+		{
+			case PlayerState.NORMAL:
+				break;
+			case PlayerState.HIDDEN:
+				break;
+			case PlayerState.GAMEOVER:
+				break;
+			default:
+				break;
+		}
 	}
 }
