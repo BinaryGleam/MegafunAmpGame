92d14871aa55dd9885e32e32aec59b97f81a0d7c
Lukas TAMAYO <luknight@hotmail.fr>
2021-03-24 20:16:38 +0100

    [LukasFlow] Full flow with title screen
    Too tired to do details
    
    The build was bugged, I fixed it with some seird shenanigans
    Also the build is lonmg

--- Assets/Scripts/Managers/GameMgr.cs
+++ Assets/Scripts/Managers/GameMgr.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameMgr : MonoBehaviour
 {
@@ -74,6 +75,6 @@ public class GameMgr : MonoBehaviour
 
     private void GameOver()
 	{
-        
+        SceneManager.LoadScene("GameoverScreen", LoadSceneMode.Single);
 	}
 }
